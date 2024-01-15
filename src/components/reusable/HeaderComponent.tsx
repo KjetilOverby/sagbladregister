d6b357959b06ed05a019ea6ad9fa825b2e634b0c
KjetilOverby <kjeoever@msn.com>
2024-01-15 21:48:28 +0100

    href

--- src/components/reusable/HeaderComponent.tsx
+++ src/components/reusable/HeaderComponent.tsx
@@ -26,18 +26,20 @@ const HeaderComponent = () => {
       <nav className="border border-x-0 border-t-0 border-gray-200 border-b-primary bg-base-100 px-4 py-2.5  lg:px-6">
         <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between">
           <div className="flex">
-            <div>
-              <a href="/" className="flex items-center">
-                <img
-                  src="https://cdn-icons-png.flaticon.com/512/9615/9615380.png"
-                  className="mr-3 h-6 sm:h-9"
-                  alt="Flowbite Logo"
-                />
-                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-                  KS-TOOLS
-                </span>
-              </a>
-            </div>
+            <Link href="/">
+              <div>
+                <p className="flex items-center">
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/9615/9615380.png"
+                    className="mr-3 h-6 sm:h-9"
+                    alt="Flowbite Logo"
+                  />
+                  <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+                    KS-TOOLS
+                  </span>
+                </p>
+              </div>
+            </Link>
           </div>
           <div className="flex">
             <ul className="mt-4 flex flex-col font-medium lg:mt-0 lg:flex-row lg:space-x-8">
